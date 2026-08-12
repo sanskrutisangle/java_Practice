dd15b38072cf152f47f71415dc67402262a94f1a
sanskrutisangle <sanskrutisangle0809@gmail.com>
2026-08-12 06:51:52 +0530

    Search for an element in the array.

---
+++ src/array/SearchElement.java
@@ -0,0 +1,27 @@
+package array;
+
+public class SearchElement {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        int search = 30;
+
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == search) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Element found");
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
