982422f81066692b353fe682dbd92039c2aa1e69
sanskrutisangle <sanskrutisangle0809@gmail.com>
2026-08-17 05:09:39 +0530

    Append a string in StringBuilder

---
+++ src/stringBuilder/Demo.java
@@ -0,0 +1,13 @@
+package stringBuilder;
+
+public class Demo {
+	public static void main(String[] args) {
+		StringBuilder sb=new StringBuilder("hello java ");
+		sb.append("here is sanskrtui");
+		sb.append(19);
+		sb.append(true);
+		System.out.println(sb);
+		
+	}
+
+}
