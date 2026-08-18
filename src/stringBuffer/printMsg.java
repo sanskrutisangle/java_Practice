eed82646c1a4d7b80cf75159046b8f665891227b
sanskrutisangle <sanskrutisangle0809@gmail.com>
2026-08-18 06:17:14 +0530

    StringBuffer simple code

---
+++ src/stringBuffer/printMsg.java
@@ -0,0 +1,10 @@
+package stringBuffer;
+
+public class printMsg {
+	public static void main(String[] args) {
+		StringBuffer sb=new StringBuffer("sanskruti");
+		sb.append("sangale");
+		System.out.println(sb);
+	}
+
+}
