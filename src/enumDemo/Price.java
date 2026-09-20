3174dc3b6a0b599448e49ef5fa5108af38aca6ef
sanskrutisangle <sanskrutisangle0809@gmail.com>
2026-09-21 05:27:29 +0530

    Practiced Java Enum methods: values(), valueOf(), toString(), ordinal(), and name().

--- src/enumDemo/Price.java
+++ src/enumDemo/Price.java
@@ -2,7 +2,19 @@ package enumDemo;
 
 public class Price {
 	public static void main(String[] args) {
-		Size s= Size.LARGE;
-		System.out.println(s.getPrice());
+		Size[] s= Size.values();
+		for(Size data: s) {
+			
+			System.out.println(data);
+		}
+		
+//		Size.valueOf("SMALL");
+		
+		Size p=Size.SMALL;
+		System.out.println(p.ordinal());
+		
+		
+//		System.out.println(s.getPrice());
 	}
+	
 }
--- src/enumDemo/Size.java
+++ src/enumDemo/Size.java
@@ -13,4 +13,14 @@ public enum Size {
 	public int getPrice() {
 		return price;
 	}
+	
+	
+	@Override
+	public String toString() {
+		if(this==LARGE) {
+			return name()+" so you have to pay extra for that also";
+		}
+		return name();
+	}
+	
 }
